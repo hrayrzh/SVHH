40fe4ca4d7c2fdd30d372cf0ea06371b8acd953c
khhayk <hayk.khalapyam@gmail.com>
2020-07-30 18:02:21 +0400

    Context menu to add folders

--- Controllers/ExplorerController.js
+++ Controllers/ExplorerController.js
@@ -1,23 +1,72 @@
 // Explrer controller class
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+
 class ExplorerController {
     
     constructor(root, view) {
         this.root = root;
         this.view = view;
         
-        this.setupEventListeners();
+        this.setupCustomEventListeners();
+        this.setupEventListeners(document.querySelector(".caret"));
     };
     
-    setupEventListeners() {
-        document.querySelector(".folders").addEventListener('dblclick', this.ctrlAddItem);
-        //document.querySelector(".folders").addEventListener('click', this.ctrlDeleteItem);
+    setupCustomEventListeners() {
+        document.getElementById("add_folder_id").addEventListener('click', this.ctrlAddItem);    
     };
-
-    ctrlAddItem = (event/*, childName, type*/) => {
-        let childName = prompt("Insert childName");
-        let type = prompt("Insert type");
-        let parent = event.target;
+    
+    setupEventListeners(item) {
+        item.addEventListener('contextmenu', (event) => {
+            // Avoid the real one
+            event.preventDefault();
+            
+            this.currentItem = event.target;
+    
+            // Show contextmenu
+            $(".custom-menu").finish().toggle(100).
+    
+            // In the right position (the mouse)
+            css({
+                top: event.pageY + "px",
+                left: event.pageX + "px"
+            });
+        });
         
+        // If the document is clicked somewhere
+        item.addEventListener("mousedown", function (e) {
+    
+            // If the clicked element is not the menu
+            if (!$(e.target).parents(".custom-menu").length > 0) {
+        
+                // Hide it
+                $(".custom-menu").hide(100);
+            }
+        });
+        
+        document.getElementById("add_folder_id").addEventListener('click', this.ctrlAddItem);
+        
+        item.addEventListener("click", function() {
+            item.querySelector(".nested").classList.toggle("active");
+            item.querySelector(".nested").classList.toggle("caret-down");
+        });    
+    };
+    
+
+    ctrlAddItem = () => {
+        let type = 'folder';
+        //console.log("add item");
+        let childName = prompt("Insert Folder Name");
+        let parent = this.currentItem;
+        console.log(parent);     
 
         // Create the file/folder item
         let newItem = (type === 'folder') ? new Folder(childName)
@@ -25,8 +74,13 @@ class ExplorerController {
 
 
         // Add the item to the UI
-        this.view.addListItem(parent, newItem, type);
+        let newListItem = this.view.addListItem(parent, newItem, type);
         
+        // Hide it AFTER the action was triggered
+        $(".custom-menu").hide(100);
+        
+
+        this.setupEventListeners(newListItem);
     };
 
     ctrlDeleteItem = (event/*, childName, type*/) => {
--- Views/ExplorerView.js
+++ Views/ExplorerView.js
@@ -3,16 +3,29 @@ class ExplorerView {
     addListItem(parent, child, type) {
         let childName = child.name;
         
+        let nested = parent.parentElement.querySelector(".nested");
+        nested.classList.add("active");
+        parent.classList.add("caret-down");
         
-        let html = '<li> ';
+        
+        
+        let html;
         if (type === 'folder') {
-            html += '<i class="fa fa-folder"; style="font-size:20px; color:lightgray;"> ' + childName + ' </i> </li>';
+            html = '<span class="caret">' + childName + '</span> <ul class="nested"> </ul>';
+            //html += '<i class="fa fa-folder"; style="font-size:20px; color:lightgray;"> ' + childName + ' </i> </li>';
         }
         else {
-            html += '<i class="fa fa-file"> ' + childName + ' </i> </li>';
+            html = '<li>' + childName + '</li>';
         }
         
-        parent.insertAdjacentHTML('beforeend', html);
+        //html = "<li>" + childName + "</li>";
+        let newChild = document.createElement('li');
+        newChild.innerHTML = html;
+        //nested.insertAdjacentHTML('beforeend', html);
+        
+        nested.appendChild(newChild);
+        
+        return newChild;
     };
     
     deleteListItem(child) {
