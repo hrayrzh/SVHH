62caa2d7b2a0dd85ba3b0dc38888af44b232d9c3
unknown <sukiasyanvarduhi@gmail.com>
2020-07-31 23:00:32 +0400

    Initial create

--- Explorer.js
+++ Explorer.js
@@ -1,15 +1,73 @@
+const ulElement = document.getElementById('ulElement');
+const explorer = document.getElementById('explorer');
+const folderList = [];
+const fileList = [];
+
 class Folder {
-    constructor(name, fileName) {
-        this.name = name
-        this.fileName = fileName
+    state = false;
+    constructor(name, folders, files) {
+        this.name = name;
+        this.folders = folders;
+        this.files = files;
+    }
+
+    createElement(ulElement) {
+        const folderNameInput = document.createElement('input');
+        folderNameInput.type = 'text';
+        folderNameInput.classList.add('folderNameInput');
+
+        folderNameInput.addEventListener('change', () => {
+            console.log('folder Input change event');
+            this.name = folderNameInput.value;
+            const folderLiElement = document.createElement('li');
+
+            ulElement.appendChild(folderLiElement);
+
+            folderLiElement.innerHTML = folderNameInput.value;
+            console.log(folderNameInput);
+            console.log(folderLiElement)
+            validatefolderName(folderLiElement);
+            folderNameInput.remove();
+
+            const folderFiles = [];
+            const foldersInFolder = [];
+            // folderLiElement.addEventListener('click', () => {
+            //     // this.state = !this.state;
+            //     // if (this.state && document.getElementById('folderImg').onclick) {
+            //     //     console.log('clicked')
+            //     // }
+
+            //     ///////Add Folder////////////
+            //     const folder = new Folder();
+            //     const inputElement = folder.createElement(ulElement);
+            //     explorer.appendChild(inputElement);
+            //     foldersInFolder.push(folder);
+            //     this.folders = foldersInFolder;
+            // })
+            //////////// Add File/////////
+            // folderLiElement.addEventListener('contextmenu', () => {
+            //     const folderFiles = [];
+            //     const file = new File();
+            //     const inputElement = file.createElement(ulElement);
+            //     explorer.appendChild(inputElement);
+            //     folderFiles.push(file);
+            //     // console.log(folderFiles);
+            //     this.files = folderFiles;
+            // })
+
+            folderLiElement.addEventListener('contextmenu', () => {
+                dragAndDrop(folderLiElement);
+            })
+        })
+        return folderNameInput
     }
 
     getName() {
-        createFolder(this.name, this.fileName);
+        return this.name
     }
 
     rename() {
-        renameFileFolder(this.name)
+
     }
 }
 
@@ -20,89 +78,125 @@ class File {
         this.content = content;
     }
 
-    getName() {
-        createFile(this.name, this.content)
+    createElement(ulElement) {
+        const fileNameInput = document.createElement('input');
+        fileNameInput.type = 'text';
+        fileNameInput.classList.add('fileNameInput');
+
+        fileNameInput.addEventListener('change', () => {
+            console.log('Input change event');
+            this.name = fileNameInput.value;
+            const fileLiElement = document.createElement('li');
+
+            ulElement.appendChild(fileLiElement);
+
+            fileLiElement.innerHTML = fileNameInput.value;
+            validateFileName(fileLiElement);
+            fileNameInput.remove();
+
+            fileLiElement.addEventListener('click', function () {
+                addTab(fileLiElement.innerHTML);
+            })
+        })
+        return fileNameInput
     }
 
-    getContent() { }
+    getName() {
+        return this.name;
+    }
 
-    rename() {
-        renameFileFolder(this.name)
+    getContent() {
+        return this.content
+    }
+
+    rename(name) {
+        this.name = name;
+        //TODO - update element name
+    }
+
+    delete(name) {
+        fileList = fileList.filter(file => file.name !== name);
     }
 
     changeContent() { }
 }
 
-const explorer = document.getElementById('explorer');
-const folderList = [];
-const fileList = [];
+function dragAndDrop(element) {
+    const div = document.createElement('div');
+    div.classList.add('dragAndDrop');
+    const editElement = document.createElement('button');
+    editElement.innerHTML = 'Edit'
+    const deleteElement = document.createElement('button');
+    deleteElement.innerHTML = 'Delete';
+
+    div.appendChild(editElement);
+    div.appendChild(deleteElement);
+    element.appendChild(div);
+
+    editElement.addEventListener('click', () => {
+        element.contentEditable = true;
+        element.addEventListener('dblclick', () => {
+            element.contentEditable = false;
+            div.remove();
+            console.log(element.contentEditable)
+        })
+    })
+
+    deleteElement.addEventListener('click', () => {
+        element.remove();
+    })
+}
 
 function createFolder() {
-    const folderName = document.createElement('input');
-    folderName.type = 'text';
-    explorer.appendChild(folderName);
-    folderName.addEventListener('change', function () {
-        const folder = document.createElement('ul');
-        const folderElement = document.createElement('li');
-        folderElement.classList.add('folderElement')
-        folderElement.id = 'folder';
-        folderElement.innerHTML = folderName.value;
-        folderName.value = '';
-        folderName.style.display = 'none';
-        folder.appendChild(folderElement);
-        explorer.appendChild(folder);
-        const myFolder = new Folder(folderElement.innerHTML)
-        folderList.push(myFolder);
-        folderElement.addEventListener('click', function () {
-            myFolder.fileName = createFile(myFolder);
-            console.log(folderList);
-        })
-        return folderList
-    })
+    const folder = new Folder();
+    const inputElement = folder.createElement(ulElement);
+    explorer.appendChild(inputElement);
+    folderList.push(folder);
+    console.log(folderList);
+
 }
 
 function createFile() {
-    const fileName = document.createElement('input');
-    fileName.type = 'text';
-    fileName.classList.add('fileName');
-    explorer.appendChild(fileName);
-    fileName.addEventListener('change', function () {
-        const file = document.createElement('ul');
-        const fileElement = document.createElement('li');
-        fileElement.id = 'file';
-        fileElement.innerHTML = fileName.value;
-        const myFile = new File(fileElement.innerHTML, {content: 'Your code here'});
-
-        if (!(fileElement.innerHTML.includes('.'))) {
-            alert('Invalid file name');
-            fileElement.innerHTML = '';
-        }
-        else if (fileElement.innerHTML.endsWith('.html')) {
-            fileElement.innerHTML = `<> ${fileElement.innerHTML}`
-        }
-        else if (fileElement.innerHTML.endsWith('.css')) {
-            fileElement.innerHTML = `# ${fileElement.innerHTML}`
-        }
-        else if (fileElement.innerHTML.endsWith('.js')) {
-            fileElement.innerHTML = `JS ${fileElement.innerHTML}`
-        }  
-
-        fileElement.addEventListener('click', function () {
-            addTab(fileElement.innerHTML);
-        })
-
-        fileList.push(myFile);
-        console.log(fileList);
-        fileName.value = '';
-        fileName.style.display = 'none';
-        file.appendChild(fileElement);
-        explorer.appendChild(file);
-    })
-    return fileList
+    const file = new File();
+    const inputElement = file.createElement(ulElement);
+    explorer.appendChild(inputElement);
+    fileList.push(file);
+    console.log(fileList);
 }
 
+function validateFileName(element) {
+    let value = element.innerHTML;
+    if (value.endsWith('.html')) {
+        element.innerHTML = `<>  ${element.innerHTML}`
+        console.log(value);
+    }
+    else if (value.endsWith('.css')) {
+        element.innerHTML = `#  ${element.innerHTML}`
+    }
+    else if (value.endsWith('.js')) {
+        element.innerHTML = `JS  ${element.innerHTML}`
+    }
+    else if (value.endsWith('.json')) {
+        element.innerHTML = `{}  ${element.innerHTML}`
+    }
+    else {
+        alert(value + ' Invalid file name');
+        element.remove();
+    }
+}
+
+function validatefolderName(element) {
+    let value = element.innerHTML;
+    if (value.includes('.') || value.startsWith('.')
+        || value.startsWith('/') || value.startsWith('#')) {
+        alert(value + ' Invalid folder name');
+        element.remove();
+    }
+}
+
+// ????///
 function renameFileFolder(name) {
-    name.addEventListener('dblclick', function () {
+    name.addEventListener('contextmenu', function () {
         let editable = document.querySelector('.folderElement');
         console.log(editable);
         editable.onclick = function () {
@@ -115,17 +209,14 @@ function renameFileFolder(name) {
 ////////// Tabs ////////
 const tab = document.querySelector('#tab');
 
-function addTab(fileName) {
+function addTab(fileNameInput) {
     const tabElement = document.createElement('button');
     const closeIcon = document.createElement('img');
     closeIcon.src = 'closeIcon.png';
-    // closeIcon.display = 'none';
     tabElement.classList.add('tabLink');
-    tabElement.innerHTML = fileName;
-
-    tabElement.addEventListener('click', function(file){
-        editor(file.content);
-        console.log(file.content)
+    tabElement.innerHTML = fileNameInput;
+    tabElement.addEventListener('click', function () {
+        editor('//Your code here');
     })
 
     tabElement.appendChild(closeIcon);
@@ -134,9 +225,9 @@ function addTab(fileName) {
 //////////Editor///////////
 const editorElem = document.querySelector('#editor');
 
- function editor(content){
+function editor(content) {
     const editorElement = document.querySelector('.textarea');
     editorElement.innerHTML = content;
     editorElem.appendChild(editorElement);
 
- }
+}
