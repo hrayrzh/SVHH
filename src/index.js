28fb5fee784ae079b7da7987c8264e1d6be5e23c
hrayr <hr.zohrabyan@gmail.com>
2020-08-03 00:34:34 +0400

    last commit in my branch

--- src/index.js
+++ src/index.js
@@ -32,22 +32,22 @@ const generateRandomID = () => Math.random().toString(36).substring(7);
 
 
 const path = {
-	"bbbb.js": generateRandomID(),
-	"index.html": generateRandomID(),
-	"style.css": generateRandomID(),
-	"aaaa.js": generateRandomID(),
-	"New Folder": {
-		"aaasaa.js": generateRandomID(),
-		"index.js": generateRandomID(),
-	},
-	"New Folder1": {
-		"aaasasaa.js": generateRandomID(),
-		"inasdex.html": generateRandomID(),
+		"bbbb.js": generateRandomID(),
+		"index.html": generateRandomID(),
+		"style.css": generateRandomID(),
+		"aaaa.js": generateRandomID(),
 		"New Folder": {
-			"aaasdasaa.js": generateRandomID(),
-			"inasddex.html": generateRandomID(),
+			"aaasaa.js": generateRandomID(),
+			"index.js": generateRandomID(),
 		},
-	},
+		"New Folder1": {
+			"aaasasaa.js": generateRandomID(),
+			"inasdex.html": generateRandomID(),
+			"New Folder": {
+				"aaasdasaa.js": generateRandomID(),
+				"inasddex.html": generateRandomID(),
+			},
+		},
 };
 
 const separateFilesFolders = folder => {
@@ -72,7 +72,7 @@ const separateFilesFolders = folder => {
 };
 
 const getFileType = fileName => {
-	const parts = fileName.split('.');
+	const parts = (fileName || []).split('.');
 	return parts[parts.length - 1];
 };
 
@@ -80,91 +80,172 @@ const getFileType = fileName => {
 const root = document.getElementById('root');
 
 
+function createFile(fileName){
+	let li = document.createElement("li");
+		if(getFileType(fileName) === "js"){
+			li.innerHTML = 
+				`<div>
+					<span>
+						<img src="./img/png/001-js.png" alt="">
+						<span>${fileName}</span>
+					</span>
+					<span>
+						<img src="./img/png/004-edit.png" alt="" class ="edit">
+						<img src="./img/png/005-trash.png" alt="" class ="trash">
+					</span>
+				</div>`;
+		}else if(getFileType(fileName) === "css"){
+			li.innerHTML =
+				`<div>
+					<span>
+						<img src="./img/png/002-css.png" alt="">
+						<span>${fileName}</span>
+					</span>
+					<span>
+						<img src="./img/png/004-edit.png" alt="" class ="edit">
+						<img src="./img/png/005-trash.png" alt="" class ="trash">
+					</span>
+				</div>`;
+		}else if(getFileType(fileName) === "html"){
+			li.innerHTML =
+				`<div>
+					<span>
+						<img src="./img/png/003-html.png" alt="">
+						<span>${fileName}</span>
+					</span>
+					<span>
+						<img src="./img/png/004-edit.png" alt="" class ="edit">
+						<img src="./img/png/005-trash.png" alt="" class ="trash">
+					</span>
+				</div>`;
+		}else{
+			li.innerHTML =
+				`<div>
+					<span>
+						<img src="./img/png/003-html.png" alt="">
+						<span>${fileName}.js</span>
+					</span>
+					<span>
+						<img src="./img/png/004-edit.png" alt="" class ="edit">
+						<img src="./img/png/005-trash.png" alt="" class ="trash">
+					</span>
+				</div>`;
+		}
+	return li;
+}
 
-function fileIterator(foldersAndFiles, root){	
-	const [folders, files] = separateFilesFolders(foldersAndFiles);
-
-		Object.keys(folders).forEach(folderName => {
-		let li = document.createElement("li");
-		let ul = document.createElement("ul");
-		
+function createFoledr(folderName){
+	let li = document.createElement("li");
 		li.innerHTML = 
-				`<img src="./img/png/openfolder.png" alt="" class="folderBranch">
-				<img src="./img/png/025-folder.png" alt="">
-				<span><span>${folderName}</span>
-				<img src="./img/png/005-trash.png" alt="" class ="trash">
-				<img src="./img/png/004-edit.png" alt="" class ="edit">
-				</span>`;
+				`<div>
+					<span>
+						<img src="./img/png/openfolder.png" alt="" class="folderBranch">
+						<img src="./img/png/025-folder.png" alt="">
+						<span>${folderName}</span>
+					</span>
+					<span>
+						<img src="./img/png/024-add.png" title="create new folder" class ="createfolder">
+						<img src="./img/png/023-new.png" title="create new file" class ="createfile">
+						<img src="./img/png/004-edit.png" title="Edit NamedNodeMap" class ="edit">
+						<img src="./img/png/005-trash.png" title="Remove" class ="trash">
+					</span>
+				</div>`;
+	return li;
+}
+
+function startRootIterator(foldersAndFiles, root, way){
+root.innerHTML=`<img src="./img/png/024-add.png" title="create new folder" class ="createfolder">
+						<img src="./img/png/023-new.png" title="create new file" class ="createfile">`;
+	fileIterator(foldersAndFiles, root, way);
+	let createFileIcons = document.getElementsByClassName("createfile");
+
+	for (let item of createFileIcons) {
+		item.addEventListener("click", e => {
+			const way = e.target.parentElement.parentElement.parentElement.getAttribute("data");
+			
+			let name = prompt("Please enter file name");
+			if(checkFolderName(path).includes(name)){
+				let name = prompt("Please enter non-existent file name");
+				alert('Please try one more time and enter non-existent file name')
+			}else{
+				console.log(`${way}.${name}`);
+				_.set(path, `${way}.${name}`, generateRandomID())
+				startRootIterator(path, root, []);
+			}
+		})
+	}
+	
+	let createFolderIcons = document.getElementsByClassName("createfolder");	
+		for (let item of createFolderIcons) {
+		item.addEventListener("click", e => {
+			const way = e.target.parentElement.parentElement.parentElement.getAttribute("data");
+			console.log(e);
+			let name = prompt("Please enter folder name");
+				_.set(path, `${way}.${name}`, {})
+				startRootIterator(path, root, []);
+		})
+	}
+	
+	
+	
+	let arrow = document.getElementsByClassName("folderBranch");
+
+	for (let item of arrow) {	
+			let ul = item.parentElement.parentElement.parentElement.lastChild;
+		item.addEventListener("click", () => {
+			if (item.className.includes('closed')){
+				item.classList.remove('closed');
+				ul.style.display = "block";
+			}else{
+				item.classList.add('closed');
+				ul.style.display = "none";
+			}
+		});
+	}
+	
+}
+
+function fileIterator(foldersAndFiles, root, way){	
+	const [folders, files] = separateFilesFolders(foldersAndFiles);
+	
+	Object.keys(folders).forEach(folderName => {
+		way.push(folderName);
+		let ul = document.createElement("ul");
+		let li = createFoledr(folderName);
+		li.setAttribute("data", way.join("."));
 		li.appendChild(ul);
-		fileIterator(folders[folderName], ul);
+		fileIterator(folders[folderName], ul, way);
 		root.appendChild(li);
+		way = [];
 	});
 	
 	Object.keys(files).forEach(fileName => {
-		let li = document.createElement("li");
-//		console.log(getFileType(fileName));
-		
-		if(getFileType(fileName) === "js"){
-			li.innerHTML = `<span><img src="./img/png/001-js.png" alt="">`;
-		}else if(getFileType(fileName) === "css"){
-			li.innerHTML = `<span><img src="./img/png/002-css.png" alt="">`;
-		}else if(getFileType(fileName) === "html"){
-			li.innerHTML = `<span><img src="./img/png/003-html.png" alt="">`;
-		}
-		li.innerHTML += 
-			`<span>${fileName}</span>
-			<img src="./img/png/005-trash.png" alt="" class ="trash">
-			<img src="./img/png/004-edit.png" alt="" class ="edit"></span>`;
-		root.appendChild(li);
+		root.appendChild(createFile(fileName));
 	});
 	
 
 }
-fileIterator(path, root);
+
+startRootIterator(path, root, []);
 
 
 
 
-let arrow = document.getElementsByClassName("folderBranch");
 
-	for (let item of arrow) {	
-		item.addEventListener("click", () => {
-			if (item.className.includes('closed')){
-				item.classList.remove('closed');
-				item.parentElement.lastChild.style.display = "block";
-			}else{
-				item.classList.add('closed');
-				item.parentElement.lastChild.style.display = "none";
-			}
-		});
+
+
+let allFolders = [];
+
+function checkFolderName(path){
+	let [folders, files] = separateFilesFolders(path);
+	let arr = Object.keys(folders);
+	allFolders = [...allFolders, ...arr];
+	for(let i of arr){
+		let z = checkFolderName(checkFolderName(folders[i]));
 	}
-
-
-let editButtons = document.getElementsByClassName("edit");
-console.log(editButtons[0].parentElement.parentElement);
-
-
-	for (let item of editButtons) {
-		item.addEventListener("click", () => {
-			let span = item.parentElement.children;
-			for(let i of span){
-				i.style.display="none";
-			}
-			let a = document.createElement("span");
-			let b = document.createElement("input");
-			let c = document.createElement("button");
-			a.appendChild(b);
-			a.appendChild(c);
-			item.parentElement.appendChild(a);
-		});
-	}
+	return allFolders;
+}
 
 
 
-//function rename(foldersAndFiles){
-//	const [folders, files] = separateFilesFolders(foldersAndFiles);
-//	console.log(this);
-//	console.log(document.getElementsByClassName("folderBranch"));
-//}
 
-//rename(path);
